3ea70acf7af1ee5e16b8d05cd35d18e90183dcf7
windows7lake <windows7lake@gmail.com>
2020-04-21 21:37:03 +0800

    change fileprovider listener to contentResolver listener

--- android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
+++ android/src/main/java/com/flutter/moum/screenshot_callback/ScreenshotCallbackPlugin.java
@@ -6,28 +6,86 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import android.content.Context;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Build;
 import android.os.FileObserver;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 //import android.util.Log;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.ArrayList;
 
 public class ScreenshotCallbackPlugin implements MethodCallHandler {
     private static MethodChannel channel;
 
-    private Handler handler;
-    private FileObserver fileObserver;
     private String TAG = "tag";
+    ;
+    private Context context;
+    private long startListenTime = 0;
+    // 运行在 UI 线程的 Handler, 用于运行监听器回调
+    private Handler uiHandler = new Handler(Looper.getMainLooper());
+    // 内部存储器内容观察者
+    private MediaContentObserver mInternalObserver;
+    // 外部存储器内容观察者
+    private MediaContentObserver mExternalObserver;
+    private static Point sScreenRealSize;
+    // 读取媒体数据库时需要读取的列, 其中 WIDTH 和 HEIGHT 字段在 API 16 以后才有
+    private static final String[] MEDIA_PROJECTIONS = {
+            MediaStore.Images.ImageColumns.DATA,
+            MediaStore.Images.ImageColumns.DATE_TAKEN,
+            MediaStore.Images.ImageColumns.WIDTH,
+            MediaStore.Images.ImageColumns.HEIGHT,
+    };
+    private static final String SORT_ORDER = MediaStore.Images.Media.DATE_ADDED + " DESC LIMIT 1";
+    // 截屏依据中的路径判断关键字
+    private static final String[] KEYWORDS = {
+            "screenshot", "screen_shot", "screen-shot", "screen shot", "screencapture",
+            "screen_capture", "screen-capture", "screen capture", "screencap", "screen_cap",
+            "screen-cap", "screen cap", "截图", "截屏", "截圖", "截屏"
+    };
+    // 已回调过的路径
+    private final static List<String> sHasCallbackPaths = new ArrayList<>();
 
+    private ScreenshotCallbackPlugin(Context context) {
+        if (context == null) throw new IllegalArgumentException("The context must not be null.");
+        this.context = context;
+        // 获取屏幕真实的分辨率
+        if (sScreenRealSize == null) {
+            sScreenRealSize = getRealScreenSize();
+            Log.d(TAG, "Screen Real Size: " + sScreenRealSize.x + " * " + sScreenRealSize.y);
+        }
+    }
+
+    /**
+     * 获取屏幕分辨率
+     */
+    private Point getRealScreenSize() {
+        Point screenSize = new Point();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        assert windowManager != null;
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        screenSize.set(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        return screenSize;
+    }
 
     public static void registerWith(Registrar registrar) {
         channel = new MethodChannel(registrar.messenger(), "flutter.moum/screenshot_callback");
-        channel.setMethodCallHandler(new ScreenshotCallbackPlugin());
+        channel.setMethodCallHandler(new ScreenshotCallbackPlugin(registrar.activeContext()));
     }
 
     @Override
@@ -35,56 +93,207 @@ public class ScreenshotCallbackPlugin implements MethodCallHandler {
         //Log.d(TAG, "onMethodCall: ");
 
         if (call.method.equals("initialize")) {
-            handler = new Handler(Looper.getMainLooper());
-            if (Build.VERSION.SDK_INT >= 29) {
-                //Log.d(TAG, "android x");
-                List<File> files = new ArrayList<File>();
-                for (Path path : Path.values()) {
-                    files.add(new File(path.getPath()));
-                }
-
-                fileObserver = new FileObserver(files, FileObserver.CREATE) {
-                    @Override
-                    public void onEvent(int event, String path) {
-                        //Log.d(TAG, "androidX onEvent");
-                        if (event == FileObserver.CREATE) {
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    channel.invokeMethod("onCallback", null);
-                                }
-                            });
-                        }
-                    }
-                };
-                fileObserver.startWatching();
-            } else {
-                //Log.d(TAG, "android others");
-                for (Path path : Path.values()) {
-                    //Log.d(TAG, "onMethodCall: "+path.getPath());
-                    fileObserver = new FileObserver(path.getPath(), FileObserver.CREATE) {
-                        @Override
-                        public void onEvent(int event, String path) {
-                            //Log.d(TAG, "android others onEvent");
-                            if (event == FileObserver.CREATE) {
-                                handler.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        channel.invokeMethod("onCallback", null);
-                                    }
-                                });
-                            }
-                        }
-                    };
-                    fileObserver.startWatching();
-                }
-            }
+            startListen();
             result.success("initialize");
         } else if (call.method.equals("dispose")) {
-            fileObserver.stopWatching();
+            stopListen();
             result.success("dispose");
         } else {
             result.notImplemented();
+        }
+    }
+
+    private static void assertInMainThread() {
+        if (Looper.myLooper() != Looper.getMainLooper()) {
+            StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+            String methodMsg = null;
+            if (elements.length >= 4) {
+                methodMsg = elements[3].toString();
+            }
+            throw new IllegalStateException("Call the method must be in main thread: " + methodMsg);
+        }
+    }
+
+    /**
+     * 启动监听
+     */
+    private void startListen() {
+        assertInMainThread();
+        // 记录开始监听的时间戳
+        startListenTime = System.currentTimeMillis();
+        // 创建内容观察者
+        mInternalObserver = new MediaContentObserver(MediaStore.Images.Media.INTERNAL_CONTENT_URI, uiHandler);
+        mExternalObserver = new MediaContentObserver(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, uiHandler);
+        // 注册内容观察者
+        context.getContentResolver().registerContentObserver(
+                MediaStore.Images.Media.INTERNAL_CONTENT_URI,
+                false,
+                mInternalObserver
+        );
+        context.getContentResolver().registerContentObserver(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                false,
+                mExternalObserver
+        );
+    }
+
+    /**
+     * 停止监听
+     */
+    private void stopListen() {
+        assertInMainThread();
+
+        // 注销内容观察者
+        if (mInternalObserver != null) {
+            try {
+                context.getContentResolver().unregisterContentObserver(mInternalObserver);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mInternalObserver = null;
+        }
+        if (mExternalObserver != null) {
+            try {
+                context.getContentResolver().unregisterContentObserver(mExternalObserver);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mExternalObserver = null;
+        }
+
+        // 清空数据
+        startListenTime = 0;
+        sHasCallbackPaths.clear();
+    }
+
+    /**
+     * 处理媒体数据库的内容改变
+     */
+    private void handleMediaContentChange(Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            // 数据改变时查询数据库中最后加入的一条数据
+            cursor = context.getContentResolver().query(
+                    contentUri, MEDIA_PROJECTIONS, null, null, SORT_ORDER
+            );
+
+            // 获取不到数据的处理
+            if (cursor == null) {
+                Log.e(TAG, "Deviant logic.");
+                return;
+            }
+            if (!cursor.moveToFirst()) {
+                Log.d(TAG, "Cursor no data.");
+                return;
+            }
+
+            // 获取各列的索引
+            int dataIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            int dateTakenIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATE_TAKEN);
+            int widthIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.WIDTH);
+            int heightIndex = cursor.getColumnIndex(MediaStore.Images.ImageColumns.HEIGHT);
+
+            // 获取行数据
+            String data = cursor.getString(dataIndex);
+            long dateTaken = cursor.getLong(dateTakenIndex);
+            int width = cursor.getInt(widthIndex);
+            int height = cursor.getInt(heightIndex);
+
+            // 处理获取到的第一行数据
+            handleMediaRowData(data, dateTaken, width, height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null && !cursor.isClosed()) cursor.close();
+        }
+    }
+
+    /**
+     * 处理获取到的一行数据
+     */
+    private void handleMediaRowData(String data, long dateTaken, int width, int height) {
+        if (checkScreenShot(data, dateTaken, width, height)) {
+            Log.d(TAG, "ScreenShot: path = " + data + "; size = " + width + " * " + height
+                    + "; date = " + dateTaken);
+            if (!checkCallback(data)) {
+                uiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        channel.invokeMethod("onCallback", null);
+                    }
+                });
+            }
+        } else {
+            // 如果在观察区间媒体数据库有数据改变，又不符合截屏规则，则输出到 log 待分析
+            Log.w(TAG, "Media content changed, but not screenshot: path = " + data
+                    + "; size = " + width + " * " + height + "; date = " + dateTaken);
+        }
+    }
+
+    /**
+     * 判断是否已回调过, 某些手机ROM截屏一次会发出多次内容改变的通知; <br/>
+     * 删除一个图片也会发通知, 同时防止删除图片时误将上一张符合截屏规则的图片当做是当前截屏.
+     */
+    private boolean checkCallback(String imagePath) {
+        if (sHasCallbackPaths.contains(imagePath)) {
+            Log.d(TAG, "ScreenShot: imgPath has done"
+                    + "; imagePath = " + imagePath);
+            return true;
+        }
+        // 大概缓存15~20条记录便可
+        if (sHasCallbackPaths.size() >= 20) {
+            sHasCallbackPaths.subList(0, 5).clear();
+        }
+        sHasCallbackPaths.add(imagePath);
+        return false;
+    }
+
+    /**
+     * 判断指定的数据行是否符合截屏条件
+     */
+    private boolean checkScreenShot(String data, long dateTaken, int width, int height) {
+        // 判断依据一: 时间判断
+        // 如果加入数据库的时间在开始监听之前, 或者与当前时间相差大于10秒, 则认为当前没有截屏
+        if (dateTaken < startListenTime || (System.currentTimeMillis() - dateTaken) > 10 * 1000) {
+            return false;
+        }
+
+        // 判断依据二: 尺寸判断
+        // 如果图片尺寸超出屏幕, 则认为当前没有截屏
+        if (sScreenRealSize != null) {
+            if (!((width <= sScreenRealSize.x && height <= sScreenRealSize.y)
+                    || (height <= sScreenRealSize.x && width <= sScreenRealSize.y))) {
+                return false;
+            }
+        }
+
+        // 判断依据三: 路径判断
+        if (TextUtils.isEmpty(data)) return false;
+        data = data.toLowerCase();
+        // 判断图片路径是否含有指定的关键字之一, 如果有, 则认为当前截屏了
+        for (String keyWork : KEYWORDS) {
+            if (data.contains(keyWork)) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * 媒体内容观察者(观察媒体数据库的改变)
+     */
+    private class MediaContentObserver extends ContentObserver {
+
+        private Uri mContentUri;
+
+        public MediaContentObserver(Uri contentUri, Handler handler) {
+            super(handler);
+            mContentUri = contentUri;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            handleMediaContentChange(mContentUri);
         }
     }
 }
